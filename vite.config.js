13f724e7a027012669d82f8ae1348a16c0d82a6b
kalhel89 <kalhel.gomez@tecsup.edu.pe>
2025-07-07 13:45:21 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,17 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
-import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            // ← los dos archivos de entrada
+            input: [
+                'resources/js/app.js',
+                'resources/css/app.css',
+            ],
             refresh: true,
         }),
-        tailwindcss(),
     ],
+    // ✅ hace que las URLs en producción sean /build/...
+    base: '/build/',
 });
